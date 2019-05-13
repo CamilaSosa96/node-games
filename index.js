7fa2007ea06494b782c9aab8a5ad00a9dc33abfa
CamilaSosa96 <camila_ines_sosa@hotmail.com>
2019-05-13 18:56:56 -0300

    Usage info updated

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 var game = process.argv[2];
+var language = process.argv[3];
 
 if (!game) {
-  console.log('usage: node-games <game>');
+  console.log('usage: node-games <game> <language>');
   console.log('');
   console.log('Games');
   console.log('- spacecraft');
   console.log('- snake');
   console.log('- tanks');
+  console.log('');
+  console.log('Languages');
+  console.log('- NO PARAM : English (Default)');
+  console.log('- es : Español');
   return;
 }
 
